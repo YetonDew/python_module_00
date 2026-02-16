04b5de9e4a24114fa896f64354fc8a7f4999f79d
YetonDew <ariasjefferson@hotmail.com>
2026-02-16 19:24:23 +0100

    ex 6

---
+++ ex6/ft_garden_summary.py
@@ -0,0 +1,10 @@
+def ft_garden_summary():
+    name = input("Enter garden name: ")
+    plants = int(input("Enter number of plants: "))
+    print("Garden: " + name)
+    print("Plants: " + str(plants))
+    print("Status: Growing well!"
+          if plants > 25 else "Status: We need more effort! :(")
+
+
+ft_garden_summary()
