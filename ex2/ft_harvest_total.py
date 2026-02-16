183d9d5009f65102db629878fa5c3877fc1f4b9d
YetonDew <ariasjefferson@hotmail.com>
2026-02-16 18:23:20 +0100

    ex2

---
+++ ex2/ft_harvest_total.py
@@ -0,0 +1,9 @@
+def ft_harvest_total():
+    dayone = int(input("Day 1 harvest: "))
+    daytwo = int(input("Day 2 harvest: "))
+    daythree = int(input("Day 3 harvest: "))
+    total = dayone + daytwo + daythree
+    print("Total harvest: " + str(total))
+
+
+ft_harvest_total()
